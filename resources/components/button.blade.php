1de7bf864089886d367e00018417275dd28e6318
gabriel <contact@gabriel-web-studio.fr>
2022-01-24 23:29:12 +0100

    Adding button and checkbox

--- resources/components/button.blade.php
+++ resources/components/button.blade.php
@@ -1,3 +1,3 @@
-<button class="{{ $color }} {{ $text }} {{ $activeColor }} text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150">
+<button class="{{ $color }} {{ $text }} {{ $activeColor }} text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 my-3 w-full ease-linear transition-all duration-150">
     {!! $label !!}
 </button>
--- resources/components/input.blade.php
+++ resources/components/input.blade.php
@@ -1,4 +1,4 @@
-<div class="mb-3 w-full @if($label == '') pt-4 @endif">
+<div class="my-3 w-full @if($label == '') pt-4 @endif">
     @include('components.label')
     <input id="{{ $name }}" name="{{ $name }}" type="{{ $type }}" @if($type == "number") step="any" @endif placeholder="{{ $placeholder }}" value="{{ old($name) ?? $value}}" @if($readonly) readonly="readonly" @endif
     class="p-2 border border-gray-800 @if(config('input.dark_mode')) bg-gray-900 text-gray-400 @else bg-white text-gray-800 @endif @if($readonly) cursor-not-allowed @endif rounded text-sm shadow focus:outline-none w-full ease-linear transition-all duration-150"/>
